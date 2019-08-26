9d4fd6d57f4f6980f2ae3e8b9107760e492df872
Sheleeza <sheleezar9@gmail.com>
2019-08-26 17:45:59 -0400

    added 5 pkgs

---
+++ server.js
@@ -0,0 +1,30 @@
+
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// Make a request via axios to grab the HTML body from the site of your choice
+axios.get("https://www.nytimes.com").then(function(response) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // Select each element in the HTML body from which you want information.
+  $("article").each(function(i, element) {
+
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
